1fcea8756d726ad6c56cdc629a7e1e273b645c2b
Aleksei Khatkevich <alekseikhatkevich@gmail.com>
2020-11-03 12:52:53 +0300

    added 3 check constraints

--- bitly/models.py
+++ bitly/models.py
@@ -1,15 +1,21 @@
 import functools
 
+from django.core.validators import MinLengthValidator
 from django.db import models
+from django.db.models import CharField
+from django.db.models.functions import Length
 from django.utils.crypto import get_random_string
-
+from short_urls import constants
 from bitly import validators
+
+CharField.register_lookup(Length)
 
 
 class UrlModel(models.Model):
     """
 
     """
+
     user_id = models.UUIDField(
         verbose_name='user unique identifier',
         db_index=True,
@@ -26,6 +32,7 @@ class UrlModel(models.Model):
         unique=True,
         max_length=7,
         default=functools.partial(get_random_string, 7),
+        validators=[MinLengthValidator(7), ],
     )
     creation_datetime = models.DateTimeField(
         verbose_name='creation datetime',
@@ -35,6 +42,20 @@ class UrlModel(models.Model):
     class Meta:
         verbose_name = 'url'
         verbose_name_plural = 'urls'
+        constraints = (
+            models.CheckConstraint(
+                name='url_code_length_check',
+                check=models.Q(url_code__length=7)
+            ),
+            models.CheckConstraint(
+                name='user_id_uuid_check',
+                check=models.Q(user_id__regex=constants.UUID4_REGEX),
+            ),
+            models.CheckConstraint(
+                name='full_url_check',
+                check=models.Q(full_url__regex=constants.URL_REGEX),
+            ),
+        )
 
     def __repr__(self):
         return f'{self.pk=} ~ {self.user_id=} ~ {self.url_code=}'
@@ -46,4 +67,3 @@ class UrlModel(models.Model):
         if fc:
             self.full_clean()
         super().save(*args, **kwargs)
-
--- short_urls/error_messages.py
+++ short_urls/error_messages.py
@@ -6,3 +6,7 @@ WRONG_UUID = error_message(
     'Строка UUID не соответсует стандарту. Допустимы только строки UUID версии 4',
     'wrong_uuid',
 )
+WRONG_URL_CODE_LEN = error_message(
+    'Длинна "url_code" должна быть ровно 7 символов',
+    'wrong_url_code_len',
+)
